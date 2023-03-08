5cbad75aa3152822eb65040b7c19cf58bc0f25be
Rubivilo <monito.r@live.com>
2023-03-08 10:18:06 +0100

    pj sprites crashea

--- personajes.py
+++ personajes.py
@@ -13,8 +13,10 @@ ABAJO = 4
 #Posturas
 SPRITE_QUIETO = 0
 SPRITE_ANDANDO = 1
+SPRITE_AGACHADO = 4
 SPRITE_SALTANDO = 2
 SPRITE_CAYENDO = 3
+SPRITE_APLASTADO = 5
 
 # Velocidades de los distintos personajes
 VELOCIDAD_JUGADOR = 0.2 # Pixeles por milisegundo
@@ -142,11 +144,12 @@ class Personaje(MiSprite):
                 self.numImagenPostura = len(self.coordenadasHoja[self.numPostura])-1
             self.image = self.hoja.subsurface(self.coordenadasHoja[self.numPostura][self.numImagenPostura])
 
-            # Si esta mirando a la izquiera, cogemos la porcion de la hoja
-            if self.mirando == IZQUIERDA:
-                self.image = self.hoja.subsurface(self.coordenadasHoja[self.numPostura][self.numImagenPostura])
+        
             #  Si no, si mira a la derecha, invertimos esa imagen
-            elif self.mirando == DERECHA:
+            if self.mirando == DERECHA:
+                self.image = self.hoja.subsurface(self.coordenadasHoja[self.numPostura][self.numImagenPostura])
+            # Si esta mirando a la izquiera, cogemos la porcion de la hoja
+            elif self.mirando == IZQUIERDA:
                 self.image = pygame.transform.flip(self.hoja.subsurface(self.coordenadasHoja[self.numPostura][self.numImagenPostura]), 1, 0)
 
 
@@ -234,7 +237,8 @@ class Jugador(Personaje):
     "Cualquier personaje del juego"
     def __init__(self):
         # Invocamos al constructor de la clase padre con la configuracion de este personaje concreto
-        Personaje.__init__(self,'Jugador.png','coordJugador.txt', [6, 12, 6], VELOCIDAD_JUGADOR, VELOCIDAD_SALTO_JUGADOR, RETARDO_ANIMACION_JUGADOR);
+        Personaje.__init__(self,'drunkSpriteSheet.png','drunkCoord.txt', [1, 3, 1, 1, 1, 1], VELOCIDAD_JUGADOR, VELOCIDAD_SALTO_JUGADOR, RETARDO_ANIMACION_JUGADOR);
+        #Personaje.__init__(self,'Jugador.png','coordJugador.txt', [6, 12, 6], VELOCIDAD_JUGADOR, VELOCIDAD_SALTO_JUGADOR, RETARDO_ANIMACION_JUGADOR);
 
 
     def mover(self, teclasPulsadas, arriba, abajo, izquierda, derecha):
