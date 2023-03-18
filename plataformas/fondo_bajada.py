33f54047f5aa3d1fbbaa78cdbcc5187da395d66c
Roimm1 <roimillan007@gmail.com>
2023-03-18 18:15:36 +0100

    Clases plataforma

---
+++ plataformas/fondo_bajada.py
@@ -0,0 +1,15 @@
+
+from plataformas.plataformas import *
+import pygame, sys, os
+from pygame.locals import *
+from gestorRecursos import *
+
+class Fondo_Bajada(Plataformas):
+    def __init__(self, x, y):
+        Plataformas.__init__(self)
+        wall_image = GestorRecursos.CargarImagen('background.png',-1)
+        self.image = wall_image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.establecerPosicion((self.rect.left, self.rect.bottom))
